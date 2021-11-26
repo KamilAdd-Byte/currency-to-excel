52111c9bc30b405daf13114d6860b0d72025832e
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-11-26 20:15:23 +0100

    0.1 Generating project and add .gitignore #1

---
+++ src/main/java/pl/creator/currencytoexcel/CurrencyToExcelApplication.java
@@ -0,0 +1,13 @@
+package pl.creator.currencytoexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyToExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyToExcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/creator/currencytoexcel/CurrencyToExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.creator.currencytoexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyToExcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
