ffb9af37e1833383be945391c08bad210f2e318a
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-11-27 17:12:23 +0100

    1.6 Exceptions handling if {fileName} is null, added loggerInterceptor class and config #9

---
+++ src/main/java/pl/creator/currencytoexcel/loggerinterceptor/config/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package pl.creator.currencytoexcel.loggerinterceptor.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pl.creator.currencytoexcel.loggerinterceptor.LoggerInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    LoggerInterceptor loggerInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggerInterceptor);
+    }
+}
