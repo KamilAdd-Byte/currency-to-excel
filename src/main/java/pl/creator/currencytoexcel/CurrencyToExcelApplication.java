0cb35f6821250e9c99a51699663ebae1fb6531c1
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-11-27 15:25:11 +0100

    1.4 Adding, running and testing rest endpoint in swagger #7

--- src/main/java/pl/creator/currencytoexcel/CurrencyToExcelApplication.java
+++ src/main/java/pl/creator/currencytoexcel/CurrencyToExcelApplication.java
@@ -2,8 +2,10 @@ package pl.creator.currencytoexcel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CurrencyToExcelApplication {
 
     public static void main(String[] args) {
